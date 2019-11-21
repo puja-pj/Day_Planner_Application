2867352e33e72a3cf505e29d13ccc180f3e46913
puja-pj <pujamullapudi465@gmail.com>
2019-11-21 14:06:01 -0500

    added files to .gitigore

--- DayPlanner_v1/DayPlanner/src/main/java/com/puja/DayPlanner/service/DealsScrapperServiceImpl.java
+++ DayPlanner_v1/DayPlanner/src/main/java/com/puja/DayPlanner/service/DealsScrapperServiceImpl.java
@@ -16,21 +16,10 @@ public class DealsScrapperServiceImpl implements DealsScrapperService{
 		
 		Map<String, String> deals = new LinkedHashMap<String, String>();
 			
-			deals.put("Amazon Canada", "Shop Early Black Friday Deals Now at Amazon.ca!");
-			deals.put("Shoppers Drug Mart","New Flyer! Royale Bathroom Tissue $4.99 + More!");
-			deals.put("IKEA","Take Up to 15% Off Select Speakers at IKEA!");
-			deals.put("Shoppers Drug Mart","Black Friday Super Sale!");
-			deals.put("Best Buy","Shop Black Friday 2019 Deals Early at Best Buy!");
-			deals.put("Canadian Tire","Canadian Tire Early Black Friday 2019 Deals!");
-			deals.put("Walmart","Walmart Canada Black Friday 2019 Flyer Early Deals");
-			deals.put("Google.com","Preview Google's Black Friday 2019 Deals Now!");
-			deals.put("Best Buy","See Best Buy's Black Friday 2019 Flyer Now on RFD!");
-			deals.put("Microsoft","Preview Microsoft's Black Friday 2019 Deals Now!");
+		String url="https://www.redflagdeals.com/in/"+city+"/deals/?search_type=popular";
 		
-			//String url="https://www.redflagdeals.com/in/"+city+"/deals/?search_type=popular";
 		
-		//deals.put("Best Buy", "Shop Black Friday 2019 Prices Now at Best Buy");
-		/*String data1 = null;
+		String data1 = null;
 		String data2 = null;
 		try {
 		Document doc = Jsoup.connect(url).get();
@@ -51,7 +40,7 @@ public class DealsScrapperServiceImpl implements DealsScrapperService{
 		}
 		catch(IOException e) {
 			e.printStackTrace();	
-		}*/
+		}
 		return deals;
 		
 	}
